f6e958bd98558203f3894603102688c5ee4eae4b
Laurel Lao Ang <laurellaoang@gmail.com>
2023-01-19 18:39:30 +0800

    Initial commit

---
+++ src/main/java/com/lao/approlemail/SendMailWithAppRoleApplication.java
@@ -0,0 +1,13 @@
+package com.lao.approlemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMailWithAppRoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendMailWithAppRoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lao/approlemail/SendMailWithAppRoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lao.approlemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMailWithAppRoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
